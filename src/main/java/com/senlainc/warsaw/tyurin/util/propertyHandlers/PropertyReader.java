b0de03ae98ce0361ad9c95a20710e10d53053e6a
Yevgany Tyurin <evgturin@gmail.com>
2023-09-05 22:34:01 +0300

    Simplify properties handling

--- src/main/java/com/senlainc/warsaw/tyurin/util/propertyHandlers/PropertyReader.java
+++ src/main/java/com/senlainc/warsaw/tyurin/util/propertyHandlers/PropertyReader.java
@@ -4,20 +4,15 @@ import com.senlainc.warsaw.tyurin.util.Constants;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 public class PropertyReader {
 
     private static PropertyReader INSTANCE;
     private Properties properties;
-    private Map<String, String> propertiesMap;
 
     private PropertyReader() {
         properties = new Properties();
-        propertiesMap = new HashMap();
         readProperties();
     }
 
@@ -30,19 +25,13 @@ public class PropertyReader {
 
     private void readProperties() {
         try (InputStream input = PropertyReader.class.getClassLoader().getResourceAsStream(Constants.PATH_TO_PROPERTIES)) {
-            System.out.println("readProperties");
             properties.load(input);
-            propertiesMap.putAll(properties.entrySet()
-                    .stream()
-                    .collect(Collectors.toMap(e -> e.getKey().toString(),
-                            e -> e.getValue().toString())));
         } catch (IOException e) {
             System.out.println("Exception occurred during reading from file " + Constants.PATH_TO_PROPERTIES.substring(Constants.PATH_TO_PROPERTIES.lastIndexOf("\\") + 1));
         }
     }
 
     public String getProperty(String key) {
-        System.out.println(propertiesMap.get(key));
-        return propertiesMap.get(key);
+        return properties.getProperty(key);
     }
 }
